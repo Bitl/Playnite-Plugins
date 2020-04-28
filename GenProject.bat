6fdcdc65601f13ac4af41f3aee3fa655d01afae1
Bitl <bitlblox@gmail.com>
2020-04-28 13:52:03 -0700

    updated

--- GenProject.bat
+++ GenProject.bat
@@ -1,13 +1,7 @@
 @ECHO OFF
 CLS
-SET ClassName="GenericPlugin"
-SET /P _inputplugintype= Choose a type of plugin to create (Generic, Library, or Metadata):
-IF "%_inputplugintype%"=="Generic" SET "%ClassName%"="GenericPlugin"
-IF "%_inputplugintype%"=="Generic" GOTO name
-IF "%_inputplugintype%"=="Library" SET "%ClassName%"="LibraryPlugin"
-IF "%_inputplugintype%"=="Library" GOTO name
-IF "%_inputplugintype%"=="Metadata" SET "%ClassName%"="MetadataPlugin"
-IF "%_inputplugintype%"=="Metadata" GOTO name
+SET /P _inputplugintype= Choose a type of plugin to create (GenericPlugin, LibraryPlugin, or MetadataPlugin):
+GOTO name
 
 :name
 CLS
@@ -16,6 +10,6 @@ GOTO create
 
 :create
 CLS
-ECHO Creating a new %ClassName% project ("%_inputpluginname%") at %CD%.
-Toolbox.exe new %ClassName% "%_inputpluginname%" "%CD%"
+ECHO Creating a new %_inputplugintype% project ("%_inputpluginname%") at %CD%.
+Toolbox.exe new "%_inputplugintype%" "%_inputpluginname%" "%CD%"
 pause
